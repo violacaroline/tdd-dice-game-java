f6b3d408213d82510b6a29435afd3a96a781ee41
Caroline Akesson <ca223pw@student.lnu.se>
2023-01-05 13:53:53 -0500

    RED: Application should have a GameEvent enum with value PLAY

---
+++ app/src/test/java/assignment/part/tdd/GameEventTest.java
@@ -0,0 +1,17 @@
+package assignment.part.tdd;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class GameEventTest {
+  /**
+   * GameEvent Enum should have a PLAY value.
+   */
+  @Test
+  void gameEventEnumShouldHavePlayValue() {
+    GameEvent value = GameEvent.findGameEventValue("PLAY");
+
+    assertEquals(GameEvent.PLAY, value);
+  }
+}
