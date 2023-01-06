5f3528f9e303b31bc268968cbb662bdcb4adc186
Caroline Akesson <ca223pw@student.lnu.se>
2023-01-05 21:45:54 -0500

    REFACTOR: Make invalid input test parameterized to test more inputs

--- app/src/test/java/assignment/part/tdd/userInterfaceTest.java
+++ app/src/test/java/assignment/part/tdd/userInterfaceTest.java
@@ -5,6 +5,8 @@ import java.io.*;
 import java.util.Scanner;
 import org.junit.jupiter.api.*;
 // import org.mockito.Mockito;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserInterfaceTest {
   private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -59,10 +61,10 @@ public class UserInterfaceTest {
     assertEquals(GameEvent.QUIT, this.userInterface.printMenu(new Scanner(inputStream)));
   }
 
-  @Test
-  void uiPrintMenuShouldReturnGameEventNothingForAnyOtherLetterThanPorQ() {
-    String correctInput = "e";
-    ByteArrayInputStream inputStream = new ByteArrayInputStream(correctInput.getBytes());
+  @ParameterizedTest
+  @ValueSource(strings = {"l", "1", "'", "/<", "something else", "-1"})
+  void uiPrintMenuShouldReturnGameEventNothingForAnyOtherLetterThanPorQ(String randomInput) {
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(randomInput.getBytes());
 
     assertEquals(GameEvent.NOTHING, this.userInterface.printMenu(new Scanner(inputStream)));
   }
