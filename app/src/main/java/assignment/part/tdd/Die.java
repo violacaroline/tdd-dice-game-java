d38e696694d451239eb160530e01e6d6f01558c7
Caroline Akesson <ca223pw@student.lnu.se>
2023-01-06 13:09:13 -0500

    GREEN: Create a roll method returning a random number between 1 and 6

--- app/src/main/java/assignment/part/tdd/Die.java
+++ app/src/main/java/assignment/part/tdd/Die.java
@@ -1,9 +1,17 @@
 package assignment.part.tdd;
 
+import java.util.Random;
+
 public class Die {
   private int value = 0;
 
   public int getValue() {
     return this.value;
+  }
+
+  public int roll() {
+    Random random = new Random();
+
+    return random.nextInt(1 + 6) + 1;
   }
 }
