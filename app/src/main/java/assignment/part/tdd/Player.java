650d7f1cf86364572b5b6d9b3ae99e81472146a3
Caroline Akesson <ca223pw@student.lnu.se>
2023-01-08 19:36:26 -0500

    GREEN: Implement a call to diceGame.playGame to start the game

--- app/src/main/java/assignment/part/tdd/Player.java
+++ app/src/main/java/assignment/part/tdd/Player.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Player {
   private UserInterface ui;
   private Scanner scan;
-  // private DiceGame diceGame;
+  private DiceGame diceGame;
 
   public Player (UserInterface ui, Scanner scan, DiceGame diceGame) {
     this.ui = ui;
     this.scan = scan;
-    // this.diceGame = diceGame;
+    this.diceGame = diceGame;
   }
 
   public void playGame() {
     ui.printWelcome();
 
     do {
-      
+      this.diceGame.playGame();
     } while (ui.printMenu(scan) != GameEvent.QUIT);
 
     ui.printQuittingMessage();
